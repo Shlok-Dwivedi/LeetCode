ba38bc7730bdcf7a953ba48bb8ca650287f154ed
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2025-08-21 22:49:19 +0530

    Time: 0 ms (100%), Space: 10.9 MB (33.07%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -0,0 +1,14 @@
+int minPathSum(int** grid, int gridSize, int* gridColSize) {
+    int m = gridSize;
+    int n = gridColSize[0];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) continue;
+            else if (i == 0) grid[i][j] += grid[i][j - 1];
+            else if (j == 0) grid[i][j] += grid[i - 1][j];
+            else grid[i][j] += (grid[i - 1][j] < grid[i][j - 1] ? grid[i - 1][j] : grid[i][j - 1]);
+        }
+    }
+    return grid[m - 1][n - 1];
+}
