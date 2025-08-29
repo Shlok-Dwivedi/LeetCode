970d6732a23d6cf37c2561c05dd0520d81545ccc
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2025-08-29 23:34:32 +0530

    Time: 1829 ms (0%), Space: 8 MB (14.92%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.c
@@ -0,0 +1,24 @@
+int countNumbersWithUniqueDigits(int n) {
+    int count = 0;
+    int limit = 1;
+    for (int i = 0; i < n; i++) limit *= 10;   // instead of 10**n
+
+    for (int i = 0; i < limit; i++) {
+        int seen[10] = {0};  // track digits used
+        int d = i;
+        int unique = 1;
+
+        while (d > 0) {
+            int digit = d % 10;
+            if (seen[digit]) {   // digit already used
+                unique = 0;
+                break;
+            }
+            seen[digit] = 1;
+            d /= 10;
+        }
+
+        if (unique) count++;
+    }
+    return count;
+}
