a33a890c238a49ee404e9bc9fafb9a37804ef366
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2025-09-16 08:05:56 +0530

    Time: 655 ms (99.88%), Space: 0B (100%) - LeetHub

---
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select person_name from(select person_name, turn, sum(weight) over(order by turn) as cumulative_weight from queue) as ce where cumulative_weight<=1000 order by turn desc limit 1;
