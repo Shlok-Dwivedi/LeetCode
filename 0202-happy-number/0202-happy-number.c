293ba324178fdbfef1b4770233f24c0f7624e368
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2025-10-02 23:03:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.61%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.c
@@ -0,0 +1,19 @@
+int getNext(int n) {
+    int total = 0, digit;
+    while (n > 0) {
+        digit = n % 10;
+        total += digit * digit;
+        n /= 10;
+    }
+    return total;
+}
+
+bool isHappy(int n) {
+    int slow = n, fast = getNext(n);
+
+    while (fast != 1 && slow != fast) {
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
+    }
+    return fast == 1;
+}
