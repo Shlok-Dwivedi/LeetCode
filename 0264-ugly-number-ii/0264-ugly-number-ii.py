f49afc7b93e774133a4cebf33e8d366f1135c06b
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-04-09 20:50:22 +0530

    Time: 38 ms (68.19%), Space: 19.3 MB (74.07%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        ugly_numbers_set = set()
-        ugly_numbers_set.add(1)
-
-        current_ugly = 1
-        for i in range(n):
-            current_ugly = min(ugly_numbers_set)
-            ugly_numbers_set.remove(current_ugly)
-            ugly_numbers_set.add(current_ugly * 2)
-            ugly_numbers_set.add(current_ugly * 3)
-            ugly_numbers_set.add(current_ugly * 5)
-        return current_ugly
+        list1=[0]*n
+        list1[0]=1
+        a=b=c=0
+        for i in range(1,n):
+            list1[i]=min(list1[a]*2,list1[b]*3,list1[c]*5)
+            if list1[a]*2==list1[i]:a+=1
+            if list1[b]*3==list1[i]:b+=1
+            if list1[c]*5==list1[i]:c+=1
+        return list1[n-1]
