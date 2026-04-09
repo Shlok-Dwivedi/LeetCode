3655a2c20817d2492c784b84d8e30f0fba9dc5fc
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-04-09 20:29:38 +0530

    Time: 2 ms (62.42%), Space: 20.3 MB (83.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        d={}
-        for i in range(len(nums)+1):
-            d[i]=0
+        v=[0]*(len(nums)+1)
         for i in nums:
-            d[i]=1
-        for i in d:
-            if d[i]==0:
+            v[i]=1
+        for i in range(len(v)):
+            if v[i]==0:
                 return i
