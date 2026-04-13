e02f64c949e1877054fbd8ead3a9fdaaa7b3eb23
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-04-13 10:38:43 +0530

    Time: 3534 ms (64.9%), Space: 19.5 MB (64.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,24 +1,38 @@
+from typing import List
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtracking(i, j, k):
-            if k == len(word):
-                return True
-            if (i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or 
-                board[i][j] != word[k]):
-                return False
-            
-            temp = board[i][j]
-            board[i][j] = ""
-            
-            if (backtracking(i+1,j,k+1) or backtracking(i-1,j,k+1) or backtracking(i,j+1,k+1) or backtracking(i,j-1,k+1)):
-                board[i][j] = temp
-                return True
-            
-            board[i][j] = temp
+        if not board or not word:
             return False
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtracking(i, j, 0):
-                    return True
+        rows = len(board)
+        cols = len(board[0])
+        
+        def search(r, c, index):
+            if index == len(word):
+                return True
+            
+            if (r < 0 or r >= rows or 
+                c < 0 or c >= cols or 
+                board[r][c] != word[index] or 
+                board[r][c] == ""):
+                return False
+            
+            temp = board[r][c]
+            board[r][c] = ""
+            
+            found = (search(r + 1, c, index + 1) or 
+                     search(r - 1, c, index + 1) or 
+                     search(r, c + 1, index + 1) or 
+                     search(r, c - 1, index + 1))
+            
+            board[r][c] = temp
+            return found
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    if search(i, j, 0):
+                        return True
+                        
         return False
