a4fa1de3097ee3a8064dd2ba3f7c9d828db1d6ab
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-05-15 10:37:37 +0530

    Time: 5 ms (78.9%), Space: 45.8 MB (86.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // If the number is negative or ends with 0 (but is not 0), it is not a palindrome
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if (x<0){
             return false;
         }
-
-        int reversed = 0;
-        int original = x;
-
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
+        int x1=x;
+        int sum=0;
+        while(x>0){
+            int r=x%10;
+            sum=sum*10+r;
+            x=x/10;
         }
-
-        return x == reversed || x == reversed / 10;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int x = 121;
-        boolean result = solution.isPalindrome(x);
-        System.out.println("Is Palindrome: " + result);
+        if (x1==sum){
+            return true;
+        }
+        return false;
     }
 }
