d11f18baec0a5a51060a7bd77bb143ba509614eb
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-06-26 09:43:52 +0530

    Time: 2 ms (99.23%), Space: 44 MB (91.56%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            char c1 = Character.toLowerCase(s.charAt(start));
+            char c2 = Character.toLowerCase(s.charAt(end));
+
+            // Skip non-alphanumeric from left
+            if (!((c1 >= 'a' && c1 <= 'z') || (c1 >= '0' && c1 <= '9'))) {
+                start++;
+                continue;
+            }
+
+            // Skip non-alphanumeric from right
+            if (!((c2 >= 'a' && c2 <= 'z') || (c2 >= '0' && c2 <= '9'))) {
+                end--;
+                continue;
+            }
+
+            if (c1 != c2) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
