5b7071a8a597e84fd56b5743a871f1323240daa8
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-06-27 12:46:31 +0530

    Time: 7 ms (1.43%), Space: 44.1 MB (74.08%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,17 +2,19 @@ class Solution {
     static boolean existsInList(List<List<Integer>> ans, List<Integer> part) {
         return ans.contains(part);
     }
-    private void helper(int[] nums, int index, List<Integer> current, List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(current));
+    private void helper(int[] nums, int index,List<Integer> current,List<List<Integer>> ans) {
+        if (!existsInList(ans, current)) {
+            ans.add(new ArrayList<>(current));
+        }
         for (int i = index; i < nums.length; i++) {
             current.add(nums[i]);
             helper(nums, i + 1, current, ans);
             current.remove(current.size() - 1);
+        }
     }
-}
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        helper(nums, 0, new ArrayList(),ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
 }
