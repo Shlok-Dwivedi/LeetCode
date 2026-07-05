824e21313ef4cfe3c03c8a27a4d0dc71615bf822
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-05 11:01:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (35.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,13 +2,14 @@ class Solution {
     int[] dp;
     public int climbStairs(int n) {
         dp = new int[n + 1];
+        Arrays.fill(dp,-1);
         return nWays(n);
     }
     int nWays(int n) {
         if (n <= 2) {
             return n;
         }
-        if (dp[n] != 0) {
+        if (dp[n] != -1) {
             return dp[n];
         }
         dp[n] = nWays(n - 1) + nWays(n - 2);
