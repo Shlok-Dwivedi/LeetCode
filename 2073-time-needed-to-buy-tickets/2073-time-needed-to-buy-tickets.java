38e569dc54a96c386534a0c5ca0dc4f497c56b17
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-07 20:47:55 +0530

    Time: 9 ms (35.87%), Space: 46.1 MB (28.34%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count=0;
+        Queue<Integer> q=new LinkedList<>();
+        for(int t:tickets){
+            q.add(t);
+        }
+        int c=Integer.MAX_VALUE;
+        while (true) {
+            int t = q.poll();
+            if (k == 0 && t == 1) {
+                count++;
+                break;
+            }
+            t--;
+            count++;
+            if (t > 0) {
+                q.add(t);
+                if (k == 0) {
+                    k = q.size() - 1;
+                } else {
+                    k--;
+                }
+            } else {
+                if (k > 0) {
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
