0cfe1c43f68de03ae40a2654c987de2636e9da31
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-08 21:10:20 +0530

    Time: 11 ms (51.89%), Space: 56 MB (20.4%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public boolean isPossible(int[] target) {
+        if (target.length == 1) {
+            return target[0] == 1;
+        }
+        long sum = 0;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : target) {
+            sum += num;
+            maxHeap.offer(num);
+        }
+        while (maxHeap.peek() > 1) {
+            int max = maxHeap.poll();
+            long restSum = sum - max;
+            if (restSum == 1) return true;
+            if (restSum == 0 || max <= restSum) return false;
+            int prev = (int)(max % restSum);
+            if (prev == 0) prev = (int)restSum;
+            if (prev == max) return false;
+
+            maxHeap.offer(prev);
+            sum = restSum + prev;
+        }
+        return true;
+    }
+}   
