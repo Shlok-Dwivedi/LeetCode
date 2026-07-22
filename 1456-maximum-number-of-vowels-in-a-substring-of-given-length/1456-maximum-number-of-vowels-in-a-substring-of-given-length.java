0d1d2bb199ba2cccb2244bc57481658d2d0c5255
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-22 22:17:36 +0530

    Time: 11 ms (90.84%), Space: 46.4 MB (49.69%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -21,9 +21,4 @@ class Solution {
         }
         return max;
     }
-
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i'
-            || c == 'o' || c == 'u';
-    }
 }
