b1fd9a5e2a09ef9ca6b21b45d086426ed5718546
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-24 21:32:23 +0530

    Time: 1 ms (86.51%), Space: 43 MB (25.3%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(current);
+                current = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                int times = countStack.pop();
+                StringBuilder temp = stringStack.pop();
+                while (times-- > 0) {
+                    temp.append(current);
+                }
+                current = temp;
+            } else {
+                current.append(ch);
+            }
+        }
+
+        return current.toString();
+    }
+}
