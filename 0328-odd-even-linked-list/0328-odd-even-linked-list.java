c2af1ad0190a68ac2bfc6bcc256a790595b6d985
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-07-25 21:40:04 +0530

    Time: 0 ms (100%), Space: 46.3 MB (55.72%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -26,7 +26,6 @@ class Solution {
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead = even;
-
         while (even != null && even.next != null) {
             odd.next = even.next;
             odd = odd.next;
@@ -34,9 +33,7 @@ class Solution {
             even.next = odd.next;
             even = even.next;
         }
-
         odd.next = evenHead;
-
         return head;
     }
 }
