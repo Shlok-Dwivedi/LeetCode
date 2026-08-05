5f38ac5911915f5064edaa29a4afc3d1a8694178
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-08-05 21:32:37 +0530

    Time: 0 ms (100%), Space: 42.8 MB (67.54%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,11 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        // StringBuilder str1=new StringBuilder();
-        // for(int i=0;i<s.length();i++){
-        //     if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-        //         str1.append(s.charAt)
-        //     }
-        // }
         return s.toLowerCase();
     }
 }
