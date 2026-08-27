8787bc29ec7dc3760d8f9923bff4b74c3a228fd7
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-08-27 21:06:54 +0530

    Time: 3 ms (58.06%), Space: 46.5 MB (33.87%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -15,7 +15,6 @@ class Solution {
         for(int i=0;i<s.length();i++){
             int targetChar = target.charAt(i)-'a';
 
-            // Save the smallest possible character greater than target[i]
             for(int j=targetChar+1;j<26;j++){
                 if(count[j]>0){
                     bestIndex = i;
@@ -26,7 +25,6 @@ class Solution {
                 }
             }
 
-            // Try to match target[i]
             if(count[targetChar]==0){
                 break;
             }
@@ -39,14 +37,12 @@ class Solution {
             return "";
         }
 
-        // Keep only prefix before the greater character
         StringBuilder ans = new StringBuilder(
             prefix.substring(0, bestIndex)
         );
 
         ans.append(bestChar);
 
-        // Add remaining characters in sorted order
         for(int i=0;i<26;i++){
             while(bestCount[i]>0){
                 ans.append((char)('a'+i));
